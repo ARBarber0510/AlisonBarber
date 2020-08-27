e662d584c5bb00496dd4162fe1c2251c4b40281a
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-27 14:45:04 -0500

    installed npm, gsap, and dogstudio.

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// var resolve = require('nodes_modules')
+// const nmResolve = require ('node-modules-resolve');
+// const realPath = nmResolve('@dogstudio/highway');
+
+
--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+const test = () => {
+  console.log("this is from the script file");
+}
+export default test;
 // Email form
 
-var submitBtn = document.querySelector("#submit-btn");
-var nameInput = document.querySelector("#name-input");
-var emailInput = document.querySelector("#email-input");
-var response = document.querySelector("#response");
+// var submitBtn = document.querySelector("#submit-btn");
+// var nameInput = document.querySelector("#name-input");
+// var emailInput = document.querySelector("#email-input");
+// var response = document.querySelector("#response");
 
-submitBtn.addEventListener("click", function(event) {
-  event.preventDefault();
+// submitBtn.addEventListener("click", function(event) {
+//   event.preventDefault();
   
-  var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
-  submissionResponseEl.textContent = response;
-});
+//   var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
+//   submissionResponseEl.textContent = response;
+// });
